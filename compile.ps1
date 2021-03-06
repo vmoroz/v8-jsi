237fcc8cd8308768a5bd567d59e827fab1e11ec0
Vladimir Morozov <vmorozov@microsoft.com>
2021-03-05 17:49:20 -0800

    Fix build script

--- compile.ps1
+++ compile.ps1
@@ -1,7 +1,4 @@
-cls
-cd E:\GitHub\vmoroz\v8-jsi
-
-& ninja -v -j 32 -C E:\GitHub\vmoroz\v8-jsi\build\v8build\v8\out\win32\x64\Debug v8jsi jsitests
+& .\localbuild.ps1 -NoSetup
 if (!$?) {
     Write-Host "Build failure, check logs for details"
     exit 1

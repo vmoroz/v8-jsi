ddbdfe830670b3a4f19ba1de6e9d8dd9f3e87bac
Vladimir Morozov <vmorozov@microsoft.com>
2021-03-23 12:12:29 -0700

    Enable more test_function code

--- src/napi/test/lib/assert_js.cpp
+++ src/napi/test/lib/assert_js.cpp
@@ -38,27 +38,94 @@ function ok(...args) {
 }
 assert.ok = ok;
 
-function innerStrictEqual(method, argLen, actual, expected, message) {
-  if (actual !== expected) {
+function innerComparison(method, compare, defaultMessage, argLen, actual, expected, message) {
+  if (!compare(actual, expected)) {
     if (argLen < 2) {
-      message = '`assert.strictEqual()` expects two or more arguments.';
+      message = `'assert.${method.name}' expects two or more arguments.`;
     } else if (message == null) {
-      message = 'Values are not struct equal';
+      message = defaultMessage;
     }
-
-    throw new AssertionError({
-      message,
-      actual,
-      expected,
-      method
-    });
+    throw new AssertionError({message, actual, expected, method});
   }
 }
 
-function strictEqual(...args) {
-  innerStrictEqual(strictEqual, args.length, ...args);
+assert.strictEqual = function strictEqual(...args) {
+  innerComparison(strictEqual, Object.is,
+    'Values are not strict equal.', args.length, ...args);
 }
-assert.strictEqual = strictEqual;
+
+assert.deepStrictEqual = function deepStrictEqual(...args) {
+  innerComparison(deepStrictEqual, isDeepStrictEqual,
+    'Values are not deep strict equal.', args.length, ...args);
+}
+
+assert.notDeepStrictEqual = function notDeepStrictEqual(...args) {
+  innerComparison(notDeepStrictEqual, negate(isDeepStrictEqual),
+    'Values must not be deep strict equal.', args.length, ...args);
+}
+
+function negate(compare) {
+  return (...args) => !compare(...args);
+}
+
+function isDeepStrictEqual(left, right) {
+  function check(left, right) {
+    if (left === right) {
+      return true;
+    }
+    if (typeof left !== typeof right) {
+      return false;
+    }
+    if (Array.isArray(left)) {
+      return Array.isArray(right) && checkArray(left, right);
+    }
+    if (typeof left === 'number') {
+      return isNaN(left) && isNaN(right);
+    }
+    if (typeof left === 'object') {
+      return (typeof right === 'object') && checkObject(left, right);
+    }
+    return false;
+  }
+
+  function checkArray(left, right) {
+    if (left.length !== right.length) {
+      return false;
+    }
+    for (let i = 0; i < left.length; ++i) {
+      if (!check(left[i], right[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function checkObject(left, right) {
+    const leftNames = Object.getOwnPropertyNames(left);
+    const rightNames = Object.getOwnPropertyNames(right);
+    if (leftNames.length !== rightNames.length) {
+      return false;
+    }
+    for (let i = 0; i < leftNames.length; ++i) {
+      if (!check(left[leftNames[i]], right[leftNames[i]])) {
+        return false;
+      }
+    }
+    const leftSymbols = Object.getOwnPropertySymbols(left);
+    const rightSymbols = Object.getOwnPropertySymbols(right);
+    if (leftSymbols.length !== rightSymbols.length) {
+      return false;
+    }
+    for (let i = 0; i < leftSymbols.length; ++i) {
+      if (!check(left[leftSymbols[i]], right[leftSymbols[i]])) {
+        return false;
+      }
+    }
+    return check(Object.getPrototypeOf(left), Object.getPrototypeOf(right));
+  }
+
+  return check(left, right);
+}
 
 )JavaScript";
 
